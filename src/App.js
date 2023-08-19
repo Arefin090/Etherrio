eeca37fb4993071978bf679146fa565e4a6e90d1
Samuel Chin <mystdreamer+git@gmail.com>
2023-08-20 05:40:57 +1000

    feat: add components to App.js
    Add new components to script.
    Removed placeholder code.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Search from './components/Search';
+import Button from './components/Button';
+import Overview from './components/Overview';
+import MoreInfo from './components/MoreInfo';
+import Transactions from './components/Transactions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className = "App">
+          <Navbar/>
+          <Search/>
+          <Overview/>
+          <MoreInfo/>
+          <Button/>
+          <Transactions/>
+        </div>
+    );
 }
 
 export default App;
