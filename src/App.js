a006b14823a8a625e52d0787806d802b689f58fc
mystdreamer <87554751+mystdreamer@users.noreply.github.com>
2023-08-20 21:27:22 +1000

    fix: initial fix
    delete conflicting code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Search from './components/Search';
-import Button from './components/Button';
 import Overview from './components/Overview';
 import MoreInfo from './components/MoreInfo';
 import Transactions from './components/Transactions';
@@ -10,12 +7,7 @@ function App() {
 
     return (
         <div className = "App">
-          <Navbar/>
-          <Search/>
           <Overview/>
-          <MoreInfo/>
-          <Button/>
-          <Transactions/>
         </div>
     );
 }
