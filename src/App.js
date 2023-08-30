5d7dd9efae490add26aaa312e38ca6dd04a75f06
Arefin090 <101785779+Arefin090@users.noreply.github.com>
2023-08-30 22:45:54 +1000

    improved fucntionality using tabs

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './global/Navbar';
 import SearchBar from './components/SearchBar';
-import TransactionHistory from './components/TransactionHistory';
-import Balance from './components/Balance';
 import Footer from './global/Footer';
 import BackToTopButton from './components/BackToTopButton';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Visualisation from './components/Visualisation';
+import BalanceAndTransactionTabs from './components/BalanceAndTransactionTabs';
 
 function App() {
   const [showDetails, setShowDetails] = useState(false);
-  const BalanceRef = useRef(null);
-  const transactionHistoryRef = useRef(null);
 
   const handleSearch = (inputValue) => {
     const hardcodedWalletAddress = "0x1234567890"; 
     if (inputValue === hardcodedWalletAddress) {
       setShowDetails(true);
-    }
-  };
-
-  const handleBalanceClick = () => {
-    if (BalanceRef.current) {
-      BalanceRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-  
-  const handleTransactionsClick = () => {
-    if (transactionHistoryRef.current) {
-      transactionHistoryRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
@@ -51,16 +36,7 @@ function App() {
             <Route path="/" element={
               <>
                 <SearchBar handleSearch={handleSearch} />
-                {showDetails && (
-                  <>
-                    <div ref={BalanceRef}>
-                      <Balance />
-                    </div>
-                    <div ref={transactionHistoryRef}>
-                      <TransactionHistory />
-                    </div>
-                  </>
-                )}
+                {showDetails && <BalanceAndTransactionTabs />}
               </>
             } />
             <Route path="/visualisation" element={<Visualisation />} />
