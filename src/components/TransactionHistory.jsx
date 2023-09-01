bdf33df9c457779662a890fbd51c9a50b576ebfe
Arefin090 <101785779+Arefin090@users.noreply.github.com>
2023-09-02 02:24:17 +1000

    init

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -97,8 +97,8 @@ export default function StickyHeadTable() {
   };
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden', marginTop: '40px' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+    <Paper sx={{ width: '100%', overflow: 'hidden', marginTop: '40px', left: 0 }}>
+      <TableContainer>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
