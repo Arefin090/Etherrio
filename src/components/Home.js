532dbab220bc7e828ac05b5a40d9e6c1aa3480e8
Samuel Chin <mystdreamer+git@gmail.com>
2023-09-04 00:03:27 +1000

    fix: footer fixed to bottom
    Converted footer code to tailwind, corrected position

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,64 +26,64 @@ function Home() {
       };
       
     return (
-      <div>
-               <Container>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                    className="flex justify-center items-center space-x-0.0"
-                  >
-                    <DiamondIcon
-                      style={{
-                        width: "70",
-                        height: "70",
-                        marginTop: "55",
-                        marginLeft: "-10px",
-                        color: "black",
-                        fontWeight: "bold",
-                      }}
-                    ></DiamondIcon>
-                    <p
-                      style={{
-                        fontSize: "80px",
-                        marginTop: "40px",
-                        fontFamily: "Light",
-                        color: "black",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Etherrio
-                    </p>
-                  </div>
-                  <div>
-                    <section
-                      style={{
-                        fontSize: "20px",
-                        fontFamily: "Light",
-                        color: "black",
-                      }}
-                    >
-                      <em>
-                        Step into the fascinating realm of Ethereum, a
-                        groundbreaking blockchain platform that has
-                        revolutionized the way we think about money, contracts,
-                        and decentralized applications.
-                      </em>
-                    </section>
-                    {showTag ? null : (
-                      <Button
-                        variant="container"
-                        style={{ backgroundColor: "#b22a00", color: "white", marginTop: "20px"}}
-                        onClick={handleButtonClick}
-                      >
-                        Getting Started
-                      </Button>
-                    )}
-                  </div>
-                </Container>
+      <div className='flex flex-col min-h-screen'>
+        <Container>
+            <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+            className="flex justify-center items-center space-x-0.0"
+            >
+            <DiamondIcon
+                style={{
+                width: "70",
+                height: "70",
+                marginTop: "55",
+                marginLeft: "-10px",
+                color: "black",
+                fontWeight: "bold",
+                }}
+            ></DiamondIcon>
+            <p
+                style={{
+                fontSize: "80px",
+                marginTop: "40px",
+                fontFamily: "Light",
+                color: "black",
+                fontWeight: "bold",
+                }}
+            >
+                Etherrio
+            </p>
+            </div>
+            <div>
+            <section
+                style={{
+                fontSize: "20px",
+                fontFamily: "Light",
+                color: "black",
+                }}
+            >
+                <em>
+                Step into the fascinating realm of Ethereum, a
+                groundbreaking blockchain platform that has
+                revolutionized the way we think about money, contracts,
+                and decentralized applications.
+                </em>
+            </section>
+            {showTag ? null : (
+                <Button
+                variant="container"
+                style={{ backgroundColor: "#b22a00", color: "white", marginTop: "20px"}}
+                onClick={handleButtonClick}
+                >
+                Getting Started
+                </Button>
+            )}
+            </div>
+        </Container>
         <Container>
             {showFeatures && <FeaturesSection />}
         </Container>
--- src/global/footer/Footer.jsx
+++ src/global/footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../.././App.css'
 import ItemsContainer from './ItemsContainer';
-import Container from '@mui/material/Container';
 import { ReactComponent as Icon1 } from './images/fb.svg';
 import { ReactComponent as Icon2 } from './images/twitter.svg';
 import { ReactComponent as Icon3 } from './images/ig.svg';
@@ -9,39 +8,41 @@ import { ReactComponent as Icon4 } from './images/yt.svg';
 import { ReactComponent as Icon5 } from './images/linkedin.svg';
 
 const Footer = () => {
-  return (<footer style={{backgroundColor:'#123397', position:'sticky', width:'100%', boxSizing: 'border-box'}}>
-    <Container maxWidth="xl">
-  <div style={{ display: 'flex', alignItems: 'center'}} className='flex justify-center items-center space-x-4'>
-      <a href="https://www.facebook.com/" target="_blank">
-        <Icon1 style={{width:"25",height:"25",marginTop:"30"}}/>
-      </a>
-      <a href="https://twitter.com/" target="_blank">
-        <Icon2 style={{width:"25",height:"25",marginTop:"30"}}/>
-      </a>
-      <a href="https://www.instagram.com/" target="_blank">
-        <Icon3 style={{width:"25",height:"25",marginTop:"30"}}/>
-      </a>
-      <a href="https://www.youtube.com/" target="_blank">
-        <Icon4 style={{width:"25",height:"25",marginTop:"30"}}/>
-      </a>
-      <a href="https://www.linkedin.com/feed/" target="_blank">
-        <Icon5 style={{width:"25",height:"25",marginTop:"30"}}/>
-      </a>
+  return (
+    <div className="bg-blue-700 border-t border-black mb-0 py-4 flex bottom-0 w-full sticky top-[100vh]">
+      <div className="container mx-auto">
+        <div className="flex items-center bg-blue-700 sticky top-0 justify-center w-full space-x-4 ">
+          <a href="https://www.facebook.com/" target="_blank">
+            <Icon1 className="w-6 h-6 mt-6" />
+          </a>
+          <a href="https://twitter.com/" target="_blank">
+            <Icon2 className="w-6 h-6 mt-6" />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank">
+            <Icon3 className="w-6 h-6 mt-6" />
+          </a>
+          <a href="https://www.youtube.com/" target="_blank">
+            <Icon4 className="w-6 h-6 mt-6" />
+          </a>
+          <a href="https://www.linkedin.com/feed/" target="_blank">
+            <Icon5 className="w-6 h-6 mt-6" />
+          </a>
+        </div>
+        <ItemsContainer />
+
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-10 text-center pt-2 text-white text-sm pb-8 ">
+          <hr className="border-t border-gray-800 w-3/4 mx-auto mb-2" />
+          <span>
+            The information provided on this website is for general informational purposes only. While we strive to keep the information up-to-date and accurate, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+          </span>
+          <span>
+            © Etherrio - All rights reserved.
+          </span>
+        </div>
+      </div>
     </div>
-    <ItemsContainer />
-    
-    <div className='grid grids-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-10 text-center pt-2 text-white text-sm pb-8'>
-    <hr className='border-t border-gray-800 w-3/4 mx-auto mb-2' />
-      <span>
-        The information provided on this website is for general informational purposes only. While we strive to keep the information up-to-date and accurate, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
-      </span>
-      <span>
-      © Etherrio - All rights reserved.
-      </span>
-    </div>
-    </Container>
-  </footer>
   );
 };
+
 
 export default Footer;
