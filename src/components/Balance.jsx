5b7d161fd35ce92a5ece0f57d28d9ab0c45e8d86
Arefin090 <101785779+Arefin090@users.noreply.github.com>
2023-10-16 16:33:25 +1100

    updated balance to use web3 api

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,47 +1,55 @@
 // Balance.jsx
 import React, { useState } from 'react';
-import { Button, Card, CardContent, Typography, Collapse } from '@mui/material';
+import { Button, Card, CardContent, Typography, Collapse, Alert } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { useBalance } from './useBalance'; // Import the custom hook
+import { useBalance } from './useBalance'; 
+//import { useProof } from './useProof'; 
+import { useGasPrice } from '../Hooks/useGasPrice'; 
+import { useBlockNumber } from '../Hooks/useBlockNumber'; 
 
-export default function Overview({ address }) { // Add address prop
+export default function Overview({ address }) { 
     const [expanded, setExpanded] = useState(false);
-    const { balance, error } = useBalance(address); // Use the custom hook
+    const { balance, error: balanceError } = useBalance(address); 
+    //const { proof, error: proofError } = useProof(address); 
+    const { gasPrice, error: gasPriceError } = useGasPrice(); 
+    const { blockNumber, error: blockError } = useBlockNumber(); 
+
+    console.log('Balance:', balance);
+    //console.log('Proof:', proof);
+    console.log('Gas Price:', gasPrice);
+    console.log('Block Number:', blockNumber);
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
 
-    // Handle loading and error states
-    if (error) {
-        return (
-            <p>Error fetching balance: {error.message}</p>
-        );
-    }
-
-    if (balance === null) {
-        return (
-            <p>Loading...</p>
-        );
-    }
-
-    // Convert Wei to ETH and format the balance
-    const balanceInEth = (balance / 1e18).toFixed(7);
+    const renderError = (errorMessage) => (
+        <Alert severity="error" style={{ marginBottom: '15px' }}>
+            {errorMessage}
+            {console.log(errorMessage)}
+        </Alert>
+    );
 
     return (
         <div style={{ marginTop: '20px', marginBottom: '50px', maxWidth: '600px', margin: 'auto' }}>
             <Card variant="outlined">
                 <CardContent>
+                    {balanceError && renderError(`Error fetching balance: ${balanceError.message}`)}
+                    {/* {proofError && renderError(`Error fetching proof: ${proofError.message}`)} */}
+                    {gasPriceError && renderError(`Error fetching gas price: ${gasPriceError.message}`)}
+                    {blockError && renderError(`Error fetching block number: ${blockError.message}`)}
+
                     <Typography variant="h5" component="div">
                         Current Balance
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        {balanceInEth} ETH
+                        {balance === null ? 'Loading...' : (balance / 1e18).toFixed(7) + ' ETH'}
                     </Typography>
                     <Button 
                         startIcon={<ExpandMoreIcon />} 
                         onClick={handleExpandClick}
                         style={{ marginTop: '10px' }}
+                        disabled={balance === null  || gasPrice === null || blockNumber === null}
                     >
                         More Info
                     </Button>
@@ -50,8 +58,17 @@ export default function Overview({ address }) { // Add address prop
                             <Typography variant="body2" color="text.secondary">
                                 Wallet Address: {address}
                             </Typography>
-                            {/* Other details remain the same */}
+                            {/* <Typography variant="body2" color="text.secondary">
+                                Balance Proof: {proof ? JSON.stringify(proof) : 'Loading...'}
+                            </Typography> */}
                             <Typography variant="body2" color="text.secondary">
+                                Current Gas Price: {gasPrice ? gasPrice + ' Gwei' : 'Loading...'}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Current Block Number: {blockNumber !== null ? blockNumber : 'Loading...'}
+                            </Typography>
+                            {/* Other details remain the same */}
+                            {/* <Typography variant="body2" color="text.secondary">
                                 Last Transaction: 2023-08-23
                             </Typography>
                             <Typography variant="body2" color="text.secondary">
@@ -59,7 +76,7 @@ export default function Overview({ address }) { // Add address prop
                             </Typography>
                             <Typography variant="body2" color="text.secondary">
                                 Name Tag: builder
-                            </Typography>
+                            </Typography> */}
                         </CardContent>
                     </Collapse>
                 </CardContent>
@@ -67,56 +84,3 @@ export default function Overview({ address }) { // Add address prop
         </div>
     );
 }
-// /* Balance list for summary data of searched wallet */ 
-
-// import React, { useState } from 'react';
-// import { Button, Card, CardContent, Typography, Collapse } from '@mui/material';
-// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-// // Overview component
-// export default function Overview() {
-//     const [expanded, setExpanded] = useState(false);
-
-//     const handleExpandClick = () => {
-//         setExpanded(!expanded);
-//     };
-
-//     // balance of wallet organized via a list. Data is hardcoded for now.
-//     return (
-//         <div style={{ marginTop: '20px', marginBottom: '50px', maxWidth: '600px', margin: 'auto' }}>
-//             <Card variant="outlined">
-//                 <CardContent>
-//                     <Typography variant="h5" component="div">
-//                         Current Balance
-//                     </Typography>
-//                     <Typography variant="body2" color="text.secondary">
-//                         23.1232345 ETH ($1235.23)
-//                     </Typography>
-//                     <Button 
-//                         startIcon={<ExpandMoreIcon />} 
-//                         onClick={handleExpandClick}
-//                         style={{ marginTop: '10px' }}
-//                     >
-//                         More Info
-//                     </Button>
-//                     <Collapse in={expanded} timeout="auto" unmountOnExit>
-//                         <CardContent>
-//                             <Typography variant="body2" color="text.secondary">
-//                                 Wallet Address: 0x1234567890123456789012345678901234567890
-//                             </Typography>
-//                             <Typography variant="body2" color="text.secondary">
-//                                 Last Transaction: 2023-08-23
-//                             </Typography>
-//                             <Typography variant="body2" color="text.secondary">
-//                                 First Transaction: 2023-01-01
-//                             </Typography>
-//                             <Typography variant="body2" color="text.secondary">
-//                                 Name Tag: builder
-//                             </Typography>
-//                         </CardContent>
-//                     </Collapse>
-//                 </CardContent>
-//             </Card>
-//         </div>
-//     );
-// }
